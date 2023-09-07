7c4e1ef24ea3b726e6396aaac87f60b7b6350550
thomaslnx <marcos.msilva10@gmail.com>
2023-09-07 10:41:29 -0300

    First configurations; Add custom colors to follow the company's color theme; Finished initial layout for the frontend AI chat; ESlint and Prettier configurations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        main: '#1d3344',
+        botAvatar: '#5ef2e8',
+        botChat: '#50d8ff',
+        botText: '#dff8fd',
+        userChat: '#ccdfef',
+        userText: '#323337',
+        inputText: '#a7cde5',
+        sendButton: '#ff6002',
+        buttonText: '#ffffff',
+      },
     },
   },
   plugins: [],
